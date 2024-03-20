5432b800b03e6eee797f7e4b71f4027c1526ee5b
Bartosz Michalski <112180048+bvrtek-dev-py@users.noreply.github.com>
2024-03-20 11:56:44 +0100

    feat: add Login component (#4)

---
+++ src/api/api.ts
@@ -0,0 +1,68 @@
+const API_URL = "http://localhost:8000/api/v1";
+
+export async function GET<T>(url: string): Promise<T> {
+  const response = await fetch(url.startsWith("https") ? url : `${API_URL}${url}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+}
+
+async function POST(url: string, body: FormData | Record<string, unknown> = {}) {
+  const isMultipart = body instanceof FormData;
+
+  const response = await fetch(`${API_URL}${url}`, {
+    method: "POST",
+    credentials: "include",
+    body: isMultipart ? body : JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to post data");
+  }
+
+  return response.json();
+}
+
+async function DELETE(url: string) {
+  const response = await fetch(`${API_URL}${url}`, {
+    credentials: "include",
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to delete data");
+  }
+
+  return response.json();
+}
+
+async function PUT<T>(url: string, body: Record<string, unknown> = {}): Promise<T> {
+  const response = await fetch(`${API_URL}${url}`, {
+    credentials: "include",
+    method: "PUT",
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to update data");
+  }
+
+  return response.json();
+}
+
+const loginAsFormData = (username: string, password: string) => {
+  const formData = new FormData();
+
+  formData.append('username', username);
+  formData.append('password', password);
+
+  return formData;
+}
+
+export const login = (username: string, password: string) => POST(
+  "/auth/login",
+  loginAsFormData(username, password)
+)
---
+++ src/components/modules/Login/Login.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Paper,
+  PasswordInput,
+  rem,
+  Text,
+  TextInput,
+  Title,
+  Tooltip,
+} from "@mantine/core";
+import { IconInfoCircle } from "@tabler/icons-react";
+import classes from "./Login.module.css";
+import { useForm } from "@mantine/form";
+import { useNavigate } from "react-router-dom";
+import { useLocalStorage } from "@mantine/hooks";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "../../../api/api.ts";
+
+export function Login() {
+  const form = useForm({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const rightSection = (
+    <Tooltip
+      label="We store your data securely"
+      position="top-end"
+      withArrow
+      transitionProps={{ transition: "pop-bottom-right" }}
+    >
+      <Text component="div" c="dimmed" style={{ cursor: "help" }}>
+        <Center>
+          <IconInfoCircle
+            style={{ width: rem(18), height: rem(18) }}
+            stroke={1.5}
+          />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+
+  const [, setOpened] = useState(false);
+
+  const [, setAccessToken] = useLocalStorage({
+    key: "access_token",
+  });
+
+  const [, setRefreshToken] = useLocalStorage({
+    key: "refresh_token",
+  });
+
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation({
+    mutationFn: () => login(form.values.username, form.values.password),
+    onSuccess: (response) => {
+      setAccessToken(response.access_token);
+      setRefreshToken(response.refresh_token);
+      navigate("/");
+    },
+  });
+
+  return (
+    <Flex justify="center">
+      <Paper shadow="xs" p="xl" className={classes.loginContainer}>
+        <Box>
+          <Center>
+            <Title order={2}>Login</Title>
+          </Center>
+        </Box>
+        <Container mt="lg" size="lg">
+          <form onSubmit={form.onSubmit(() => mutate())}>
+            <TextInput
+              rightSection={rightSection}
+              label="Email"
+              required
+              placeholder="Your email"
+              {...form.getInputProps("username")}
+            />
+            <PasswordInput
+              label="Password"
+              required
+              placeholder="Your password"
+              onFocus={() => setOpened(true)}
+              onBlur={() => setOpened(false)}
+              mt="md"
+              {...form.getInputProps("password")}
+            />
+            <Button
+              mt="md"
+              type="submit"
+              variant="primary"
+              style={{ width: "100%" }}
+            >
+              Login
+            </Button>
+          </form>
+        </Container>
+      </Paper>
+    </Flex>
+  );
+}
